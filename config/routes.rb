275a41d3a92f87ca071b02421bfd9901660919a4
Rajesh <rajeshupwk@gmail.com>
2017-05-25 23:28:59 +0530

    change home page for api documentation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount API::Base, at: '/api'
 
-  get '/api' => redirect('/swagger/dist/index.html?url=/apidocs.json')
+  get '/' => redirect('/swagger/dist/index.html?url=/apidocs.json')
   resources :apidocs, only: [:index]
   
 end
